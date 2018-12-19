44c46a0daf954c979ea29e00414a287510ef591a
wey <149201216@qq.com>
2018-12-19 23:36:54 +0800

    geo component in header

---
+++ components/public/header/geo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"/>广州市
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity">切换城市</nuxt-link>
+    [天河 珠江新城 K11]
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css">
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,23 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header>
+      <my-header/>
+    </el-header>
     <el-main>
       <nuxt/>
     </el-main>
     <el-footer>Footer</el-footer>
   </el-container>
 </template>
+
+<script>
+  import MyHeader from '@/components/public/header/index.vue'
+  export default {
+    components: {
+      MyHeader
+    }
+  }
+</script>
 
 <style>
 html {
