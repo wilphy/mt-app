717c1cede0e4893454aa8f4d5327c9c0d40b200d
wey <149201216@qq.com>
2018-12-27 23:49:07 +0800

    static searchbar

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -11,7 +11,61 @@
       <el-col
         :span="15"
         class="center">
-        2
+        <div class="wrapper">
+          <el-input placeholder="搜索商家，地点······"/>
+          <button class="el-button el-button-primary"><i class="el-icon-search"/></button>
+          <dl
+            class="hotPlace"
+            style="display: none">
+            <dt>虾饺</dt>
+            <dt>烧麦</dt>
+            <dt>肠粉</dt>
+            <dt>叉烧包</dt>
+            <dt>凤爪</dt>
+          </dl>
+          <dl
+            class="searchList"
+            style="display: none">
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+        </div>
+        <p class="suggset">
+          <a href="#">广州塔</a>
+          <a href="#">广州塔</a>
+          <a href="#">广州塔</a>
+          <a href="#">广州塔</a>
+        </p>
+        <ul class="nav">
+          <li>
+            <nuxt-link
+              to="/"
+              class="takeout">美团外卖</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="movie">猫眼电影</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="hotel">酒店</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="apartment">公寓</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="business">商家入驻</nuxt-link>
+          </li>
+        </ul>
       </el-col>
       <el-col
         :span="6"
