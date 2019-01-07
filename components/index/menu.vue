779a42e44d41daf05ccc032e75aa7fe6e4317a98
wey <149201216@qq.com>
2019-01-07 23:41:45 +0800

    index nav

---
+++ components/index/menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav">
+      <dt>全部分类</dt>
+      <dd
+        v-for="(item, index) in menu"
+        :key="index">
+        <i :class="item.type"/>{{ item.title }}<span class="arrow"/>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [{
+        type: 'food',
+        title: '美食'
+      },{
+        type: 'takeout',
+        title: '外卖'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/index/index.scss"
+</style>
