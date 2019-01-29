b561e2f6fd6e2897963098a92b9fb1cd6c671380
wey <149201216@qq.com>
2019-01-29 23:46:57 +0800

    home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,29 @@
       <el-col :span="5">
         <emenu/>
       </el-col>
-      <el-col :span="19">1</el-col>
+      <el-col :span="19">
+        <life/>
+      </el-col>
     </el-row>
-    <el-row :span="24">
-      333
+    <el-row >
+      <el-col :span="24">
+        <artistic/>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
 export default {
   components: {
-    Emenu
+    Emenu,
+    Life
   }
 }
 </script>
 
+<style lang="scss">
+  @import "@/assets/css/index/index.scss";
+</style>
