4883c925436e586b485e0dcb47adcb7eb4cd83b9
wey <149201216@qq.com>
2019-02-26 20:46:50 +0800

    add register page

---
+++ pages/register.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-register">
+    <section class="table">
+      <h2>注册</h2>
+      <div class="account">
+        <label for="">账号</label>
+        <input type="text">
+      </div>
+      <div class="password">
+        <label for="">密码</label>
+        <input type="password">
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blank'
+}
+</script>
+
+<style lang="css">
+  .page-register{
+    background-color: #aaf;
+    text-align: center;
+  }
+</style>
